911d74111dd69ba5961aa724fe515a894c4f09ad
root <root@ip-172-31-39-134.ap-northeast-1.compute.internal>
2018-11-29 10:16:38 +0000

    * The version is for Ubuntu16.04

--- install_server.sh
+++ install_server.sh
@@ -2,11 +2,14 @@
 #VPN_HOST=192.168.2.101
 VPN_HOST=$1
 
-apt-get install strongswan strongswan-plugin-af-alg strongswan-plugin-agent strongswan-plugin-certexpire strongswan-plugin-coupling strongswan-plugin-curl strongswan-plugin-dhcp strongswan-plugin-duplicheck strongswan-plugin-eap-aka strongswan-plugin-eap-aka-3gpp2 strongswan-plugin-eap-dynamic strongswan-plugin-eap-gtc strongswan-plugin-eap-mschapv2 strongswan-plugin-eap-peap strongswan-plugin-eap-radius strongswan-plugin-eap-tls strongswan-plugin-eap-ttls strongswan-plugin-error-notify strongswan-plugin-farp strongswan-plugin-fips-prf strongswan-plugin-gcrypt strongswan-plugin-gmp strongswan-plugin-ipseckey strongswan-plugin-kernel-libipsec strongswan-plugin-ldap strongswan-plugin-led strongswan-plugin-load-tester strongswan-plugin-lookip strongswan-plugin-ntru strongswan-plugin-pgp strongswan-plugin-pkcs11 strongswan-plugin-pubkey strongswan-plugin-radattr strongswan-plugin-sshkey strongswan-plugin-systime-fix strongswan-plugin-whitelist strongswan-plugin-xauth-eap strongswan-plugin-xauth-generic strongswan-plugin-xauth-noauth strongswan-plugin-xauth-pam strongswan-pt-tls-client haveged 
+apt-get install strongswan strongswan-plugin-af-alg strongswan-plugin-agent strongswan-plugin-certexpire strongswan-plugin-coupling strongswan-plugin-curl strongswan-plugin-dhcp strongswan-plugin-duplicheck strongswan-plugin-eap-aka strongswan-plugin-eap-aka-3gpp2 strongswan-plugin-eap-dynamic strongswan-plugin-eap-gtc strongswan-plugin-eap-mschapv2 strongswan-plugin-eap-peap strongswan-plugin-eap-radius strongswan-plugin-eap-tls strongswan-plugin-eap-ttls strongswan-plugin-error-notify strongswan-plugin-farp strongswan-plugin-fips-prf strongswan-plugin-gcrypt strongswan-plugin-gmp strongswan-plugin-ipseckey strongswan-plugin-kernel-libipsec strongswan-plugin-ldap strongswan-plugin-led strongswan-plugin-load-tester strongswan-plugin-lookip strongswan-plugin-ntru strongswan-plugin-pgp strongswan-plugin-pkcs11 strongswan-plugin-pubkey strongswan-plugin-radattr strongswan-plugin-sshkey strongswan-plugin-systime-fix strongswan-plugin-whitelist strongswan-plugin-xauth-eap strongswan-plugin-xauth-generic strongswan-plugin-xauth-noauth strongswan-plugin-xauth-pam haveged
+
 
 systemctl enable haveged
 systemctl start haveged
 
+cp ipsec.conf /etc
+cp ipsec.secrets /etc
 cd /etc/ipsec.d/
 mkdir -p private ; rm -rf private/*
 mkdir -p cacerts ; rm -rf cacerts/*
@@ -28,5 +31,3 @@ chmod 600 private/vpnHostKey.der
 ipsec pki --pub --in private/vpnHostKey.der --type rsa | ipsec pki --issue --lifetime 730 --cacert cacerts/strongswanCert.der --cakey private/strongswanKey.der --dn "C=TW, O=Company, CN=vpn.example.org" --san vpn.example.com --san vpn.example.net --san ${VPN_HOST}  --san @${VPN_HOST} --flag serverAuth --flag ikeIntermediate --outform der > certs/vpnHostCert.der
 
 #
-cp ipsec.conf /etc
-cp ipsec.secrets /etc
---
+++ iptables.sh
@@ -0,0 +1,38 @@
+#user_ip=10.10.10.0/24
+user_ip=$1
+echo 1 > /proc/sys/net/ipv4/ip_forward
+
+iptables -F
+iptables -X
+iptables -Z
+iptables -t nat -F
+iptables -t nat -X
+iptables -t nat -Z
+iptables -t mangle -F
+iptables -t mangle -X
+iptables -t mangle -Z
+
+
+# keep connect , accept 22 for ssh
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+
+# accept local 
+iptables -A INPUT -i lo -j ACCEPT
+
+# accept ipsec 
+iptables -A INPUT -p udp --dport 500 --j ACCEPT
+iptables -A INPUT -p udp --dport 4500 --j ACCEPT
+iptables -A INPUT -p esp -j ACCEPT
+
+iptables -A FORWARD --match policy --pol ipsec --dir in  --proto esp -s ${user_ip} -j ACCEPT
+iptables -A FORWARD --match policy --pol ipsec --dir out --proto esp -d ${user_ip} -j ACCEPT
+
+iptables -t nat -A POSTROUTING -s ${user_ip} -o eth0 -m policy --pol ipsec --dir out -j ACCEPT
+iptables -t nat -A POSTROUTING -s ${user_ip} -o eth0 -j MASQUERADE
+
+iptables -t mangle -A FORWARD --match policy --pol ipsec --dir in -s ${user_ip} -o eth0 -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:1536 -j TCPMSS --set-mss 1360
+
+
+#iptables -A INPUT -j DROP
+#iptables -A FORWARD -j DROP
